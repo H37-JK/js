036ac388fc5bdfd952d381b482587e971a4076b9
these9907 <these9907@moveint.io>
2025-11-14 16:31:15 +0900

    update

--- on.js
+++ on.js
@@ -1,7 +1,7 @@
 import puppeteer from "puppeteer";
 
 const browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
     args: ['--no-sandbox', '--disable-setuid-sandbox']
 });
 
