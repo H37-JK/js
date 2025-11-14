f5d8b4ebc33ee01da7f24c57e96155317893084e
these9907 <these9907@moveint.io>
2025-11-14 16:34:05 +0900

    update

--- on.js
+++ on.js
@@ -37,7 +37,7 @@ await page.click('#mypage_myinfo > table > tbody > tr:nth-child(3) > td:nth-chil
 await page.click('#mypage_myinfo > form > div > p.right > a:nth-child(2) > img')
 
 
-for(let i = 1; i <= 100; i ++) {
+for(let i = 2; i <= 100; i ++) {
     let time = 0;
     if (i === 1) {
         await delay(1000)
@@ -45,7 +45,7 @@ for(let i = 1; i <= 100; i ++) {
         await delay(1810000)
     }
     console.log(getKSTTimeString() + ':' + `${i}번째 실행`)
-    await page.goto('https://ondisk.co.kr/index.php?mode=my_page&sm=regist&orderis=purchase_cnt&sort=DESC&code_cate=all&code=all&sch_state=&list_max=20&p=7')
+    await page.goto(`https://ondisk.co.kr/index.php?mode=my_page&sm=regist&orderis=purchase_cnt&sort=DESC&code_cate=all&code=all&sch_state=&list_max=20&p=${i}`)
 
     await page.click('#mypage_myinfo > table > tbody > tr:nth-child(3) > td:nth-child(1) > input[type=checkbox]')
     await page.click('#mypage_myinfo > form > div > p.right > a:nth-child(2) > img')
